f0d1e6a5cf8204057ac1725564714bc95804ca81
Cenita <411170425@qq.com>
2018-10-04 00:01:10 +0800

    前端尝试更改结构和提交

--- WebFront-End/backstage/nav/index.php
+++ WebFront-End/backstage/nav/index.php
@@ -7,7 +7,6 @@ $page=$_GET["page"];?>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <script src="../../lib/tool/jquery.min.js"></script>
     <script src="../../lib/tool/vue.min.js"></script>
     <link rel="stylesheet" href="../../lib/tool/bootstrap.min.css">
     <link rel="stylesheet" href="../nav/nav.css">
@@ -21,6 +20,7 @@ $page=$_GET["page"];?>
                     {
                         el:"#bnav .nav-tabs",
                         data:{
+                            index:false,
                             page1:false,
                             page2:false,
                             page3:false,
@@ -41,15 +41,18 @@ $page=$_GET["page"];?>
                 {nav.page4=true;}
                 else if(page=="Operation")
                 {nav.page5=true;}
+                else if(page=="Index")
+                {nav.index=true;}
             }
         )
     </script>
     <div id="bnav">
         <div class="logo">
-            <a href="../index/index.php"><img src="../../lib/img/logo2.png" alt=""></a>
-        </div>
+            <a href="../../public/index/index.php"><img src="../../lib/img/logo2.png" alt=""></a>
+        </div button>
         <div class="nav selectPart">
             <ul class="nav nav-tabs">
+                <li role="presentation" v-bind:class="{ active: index }"><a href="../index/index.php">首页</a></li>
                 <li role="presentation" v-bind:class="{ active: page1 }"><a href="../questionManage/index.php">题库管理</a></li>
                 <li role="presentation" v-bind:class="{ active: page2 }"><a href="../crewManage/index.php">人员管理</a></li>
                 <li role="presentation" v-bind:class="{ active: page3 }"><a href="../paperManage/index.php">试卷管理</a></li>
