e0b839b6b5577fd4f50166631a1beaf4d19eff03
杨容光 <825833848@qq.com>
2018-10-05 21:59:09 +0800

    新增了文件的增删改查接口和试卷的增删改查接口

---
+++ Web/src/hctest/domain/Paper.java
@@ -0,0 +1,114 @@
+package hctest.domain;
+
+import java.util.Date;
+
+public class Paper {
+    private String id;
+    private String title;
+    private String number;
+    private String direction;
+    private int score;
+    private int grade;
+    private int permit;
+    private String questionlist;
+    private Date createtime;
+    private Date updatetime;
+    private Date starttime;
+    private Date endtime;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getNumber() {
+        return number;
+    }
+
+    public void setNumber(String number) {
+        this.number = number;
+    }
+
+    public String getDirection() {
+        return direction;
+    }
+
+    public void setDirection(String direction) {
+        this.direction = direction;
+    }
+
+    public int getScore() {
+        return score;
+    }
+
+    public void setScore(int score) {
+        this.score = score;
+    }
+
+    public int getGrade() {
+        return grade;
+    }
+
+    public void setGrade(int grade) {
+        this.grade = grade;
+    }
+
+    public int getPermit() {
+        return permit;
+    }
+
+    public void setPermit(int permit) {
+        this.permit = permit;
+    }
+
+    public String getQuestionlist() {
+        return questionlist;
+    }
+
+    public void setQuestionlist(String questionlist) {
+        this.questionlist = questionlist;
+    }
+
+    public Date getCreatetime() {
+        return createtime;
+    }
+
+    public void setCreatetime(Date createtime) {
+        this.createtime = createtime;
+    }
+
+    public Date getUpdatetime() {
+        return updatetime;
+    }
+
+    public void setUpdatetime(Date updatetime) {
+        this.updatetime = updatetime;
+    }
+
+    public Date getStarttime() {
+        return starttime;
+    }
+
+    public void setStarttime(Date starttime) {
+        this.starttime = starttime;
+    }
+
+    public Date getEndtime() {
+        return endtime;
+    }
+
+    public void setEndtime(Date endtime) {
+        this.endtime = endtime;
+    }
+}
